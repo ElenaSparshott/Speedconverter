938c44e07a907e03319ec32379ba808d3b00b471
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-04-03 17:54:49 +0100

    calculate simple interest

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,17 @@ public class Main {
         for(int counter = 1; counter <= 5; counter++) {
             System.out.println(counter);
         }
+
+        System.out.println(calculateInterest(10000.0, 2.0));
+
     }
+
+    public static double calculateInterest (double amount, double interestRate) {
+        return (amount *(interestRate / 100));
+    }
+
+
 }
+
+
 
