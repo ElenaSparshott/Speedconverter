c281f976f4060747a3792f5249b4de9503ed4c7d
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-04-04 17:30:07 +0100

    prime checker

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ public class Main {
             System.out.println(counter);
         }
 
-        for(double rate = 2.0; rate <= 5.0; rate++){
-            double interestAmount = calculateInterest(10000.0, rate);
-            System.out.println("£10,000 at "+ rate + "% rate of interest =" + interestAmount);
+        for(double rate = 7.5; rate <= 10.0; rate+= 0.25){
+            double interestAmount = calculateInterest(100.0, rate);
+            if(interestAmount > 8.5){
+                break;
+            }
+            System.out.println("$100.00 at "+ rate + "% rate of interest = $ " + interestAmount);
         }
     }
 
---
+++ src/Prime.java
@@ -0,0 +1,12 @@
+public class Prime {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPrime(int wholeNumber){
+
+        return true;
+
+    }
+}
