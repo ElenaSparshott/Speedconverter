d552f19b2c8cf0a111ca5df1d0d73802fbbb88c3
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-04-04 18:04:52 +0100

    code challenge check range of number for prime numbers

--- src/Prime.java
+++ src/Prime.java
@@ -1,19 +1,25 @@
 public class Prime {
 
     public static void main(String[] args) {
-        System.out.println("0 is "+ (isPrime(0) ? "" : "NOT ") + "a prime number");
-        System.out.println("1 is "+ (isPrime(1) ? "" : "NOT ") + "a prime number");
-        System.out.println("2 is "+ (isPrime(2) ? "" : "NOT ") + "a prime number");
-        System.out.println("3 is "+ (isPrime(3) ? "" : "NOT ") + "a prime number");
-        System.out.println("8 is "+ (isPrime(8) ? "" : "NOT ") + "a prime number");
-        System.out.println("17 is "+ (isPrime(17) ? "" : "NOT ") + "a prime number");
+
+        int primeNumberCounter = 0;
+
+        for(int number = 0; number <= 50; number++){
+            if(isPrime(number)){
+                System.out.println(number);
+                primeNumberCounter += 1;
+                if (primeNumberCounter == 3){
+                    break;
+                }
+            }
+        }
     }
 
     public static boolean isPrime(int wholeNumber){
         if (wholeNumber <= 2) {
             return (wholeNumber == 2);
         }
-        for( int divisor = 2; divisor < wholeNumber / 2; divisor++){
+        for( int divisor = 2; divisor < wholeNumber; divisor++){
             if(wholeNumber % divisor == 0){
                 return false;
             }
